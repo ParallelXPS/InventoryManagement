d7eeedf0e623ecd530109eb897cc4fd95edddd2e
Adam Zell <zellster@gmail.com>
2017-09-11 11:44:04 -0700

    Start mapping GraphQL schema to scripts.

--- backend/graphql/src/test/java/com/parallelxps/graphql/inventory/ApplicationTest.java
+++ backend/graphql/src/test/java/com/parallelxps/graphql/inventory/ApplicationTest.java
@@ -8,12 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
 
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
   @Inject
   private TestRestTemplate template;
