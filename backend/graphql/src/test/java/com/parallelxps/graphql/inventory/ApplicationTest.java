6a6b83f3a70d40ccf0dcbd82ca914de09bbdeb3e
Adam Zell <zellster@gmail.com>
2017-09-10 23:38:12 -0700

    Provide a basic GraphQL endpoint.

--- backend/graphql/src/test/java/com/parallelxps/graphql/inventory/ApplicationTest.java
+++ backend/graphql/src/test/java/com/parallelxps/graphql/inventory/ApplicationTest.java
@@ -2,7 +2,6 @@ package com.parallelxps.graphql.inventory;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,15 +15,11 @@ import static org.junit.Assert.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApplicationTest {
-  @LocalServerPort
-  private int port;
-
   @Inject
   private TestRestTemplate template;
 
   @Test
   public void shouldSetFields() {
-    assertThat(port, not(0));
     assertThat(template, notNullValue());
   }
 }
