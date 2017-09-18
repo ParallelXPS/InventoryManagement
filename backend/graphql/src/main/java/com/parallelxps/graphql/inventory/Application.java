7d4f3dc1170b473bcb02aab963b2126f802ab97d
Adam Zell <zellster@gmail.com>
2017-09-18 02:33:59 -0700

    Start on wiring run-time database.

--- backend/graphql/src/main/java/com/parallelxps/graphql/inventory/Application.java
+++ backend/graphql/src/main/java/com/parallelxps/graphql/inventory/Application.java
@@ -1,11 +1,25 @@
 package com.parallelxps.graphql.inventory;
 
+import org.jooq.Configuration;
+import org.jooq.SQLDialect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+import static com.parallelxps.graphql.inventory.data.sql.JooqConfiguration.JooqConfiguration;
 
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public Configuration configuration(
+      DataSource dataSource, @Value("${spring.jooq.sql-dialect}") String dialect) {
+    return JooqConfiguration.configuration(dataSource, SQLDialect.valueOf(dialect));
   }
 }
--- backend/graphql/src/main/java/com/parallelxps/graphql/inventory/resolvers/QueryResolver.java
+++ backend/graphql/src/main/java/com/parallelxps/graphql/inventory/resolvers/QueryResolver.java
@@ -3,11 +3,8 @@ package com.parallelxps.graphql.inventory.resolvers;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.parallelxps.schema.inventory.tables.pojos.Categories;
 import org.jooq.DSLContext;
-import org.jooq.Record;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 import static com.parallelxps.schema.inventory.Tables.CATEGORIES;
 
@@ -21,11 +18,9 @@ public class QueryResolver implements GraphQLQueryResolver {
   }
 
   public Categories category(String id) {
-    Record record = create.selectFrom(CATEGORIES)
+    return create.selectFrom(CATEGORIES)
         .where(CATEGORIES.ID.eq(Integer.valueOf(id)))
-        .fetchOne();
-
-    return Optional.ofNullable(record)
+        .fetchOptional()
         .map(r -> r.into(Categories.class))
         .orElse(null);
   }
