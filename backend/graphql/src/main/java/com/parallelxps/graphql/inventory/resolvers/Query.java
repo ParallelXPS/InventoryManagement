dc602b412854e11b7bfe5781b1619e1db93d18a2
Adam Zell <zellster@gmail.com>
2017-09-23 23:46:40 -0700

    Continue adding to the query.

--- backend/graphql/src/main/java/com/parallelxps/graphql/inventory/resolvers/Query.java
+++ backend/graphql/src/main/java/com/parallelxps/graphql/inventory/resolvers/Query.java
@@ -1,12 +1,14 @@
 package com.parallelxps.graphql.inventory.resolvers;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.parallelxps.schema.inventory.tables.pojos.Brands;
 import com.parallelxps.schema.inventory.tables.pojos.Categories;
 import com.parallelxps.schema.inventory.tables.pojos.Keywords;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.parallelxps.schema.inventory.Tables.BRANDS;
 import static com.parallelxps.schema.inventory.Tables.CATEGORIES;
 import static com.parallelxps.schema.inventory.Tables.KEYWORDS;
 
@@ -17,6 +19,14 @@ public class Query implements GraphQLQueryResolver {
 
   public Query(DSLContext create) {
     this.create = create;
+  }
+
+  public Brands brand(String id) {
+    return create.selectFrom(BRANDS)
+        .where(BRANDS.ID.eq(pk(id)))
+        .fetchOptional()
+        .map(r -> r.into(Brands.class))
+        .orElse(null);
   }
 
   public Categories category(String id) {
